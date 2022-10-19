a77d49b850cd27a5ff8005dfb95359b5b9c27496
Max Parry <66670928+Buggem@users.noreply.github.com>
2022-10-19 15:42:21 +1100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -81,6 +81,7 @@ window.gn9_game = p => {
     cbr = canzy.elt.getBoundingClientRect();
     canzy.position(0-cbr.x, 0-cbr.y);
     canzyWinResizable();
+    p.pixelDensity(1);
   };
 
   p.draw = function() {
@@ -90,8 +91,9 @@ window.gn9_game = p => {
     p.imageMode(p.CORNER);
     p.image(images.cursor, p.mouseX, p.mouseY, 50, 50);
     p.imageMode(p.CENTER);
-    image2(images.logo, (p.width/2)-(images.logo.width/2), (p.height/5)-(images.logo.height/2), images.logo.width*6, images.logo.height*6);
-    
+    p.pixelDensity(6);
+    p.image(images.logo, (p.width/2), (p.height/5), images.logo.width*6, images.logo.height*6);
+    p.pixelDensity(1);
   };
 };
 
